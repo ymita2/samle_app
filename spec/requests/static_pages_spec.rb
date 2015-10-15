230d7f96e7aebddd6e5f7558eff4f18a8c935e6e
ymita2 <yuko.mita@unisys.co.jp>
2015-10-15 15:22:50 +0900

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Samle App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Samle App')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Samle App | Home") 
+    end    
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Samle App | Help")
+    end
+
+
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Samle App | About")
+    end
+  end
+end
