24c7dfdb1f052e31767f0eeb80546958a1c20383
ymita2 <yuko.mita@unisys.co.jp>
2015-11-04 11:10:41 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 SamleApp::Application.routes.draw do
-  get "users/new"
-
+  resources :users
   root  'static_pages#home'
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
+  match 'signup',  to: 'users#new',            via: 'get'
+  match 'help',    to: 'static_pages#help',    via: 'get'
+  match 'about',   to: 'static_pages#about',   via: 'get'
+  match 'contact', to: 'static_pages#contact', via: 'get'
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -60,4 +61,4 @@ SamleApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+ factory :user do
+  name     "Yuko Mita"
+  email    "yuko.mita@unisys.co.jp"
+  password "foobar"
+  password_confirmation "foobar"
+ end
+end
